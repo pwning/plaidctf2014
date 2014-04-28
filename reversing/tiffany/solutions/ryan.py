cff55ad9141d9de9fd774569935a7e559a8173cb
Ryan Goulden <percontation@gmail.com>
2014-04-27 23:49:53 -0400

    .

--- reversing/tiffany/solutions/ryan.py
+++ reversing/tiffany/solutions/ryan.py
@@ -1,17 +1,17 @@
 from os import environ
 from struct import unpack
 
-def readint(s):
-	return unpack('<I', s)[0]
-
-def readlong(s):
-	return unpack('<Q', s)[0]
-
 sizeoff = 0x1a0c0
 ptrsoff = 0x1a0e0
 rodiff = 0x400000
 
 pt = open(environ['HOME']+"/pctf2014/problems/reversing/pt/pt").read()
+
+def readint(s):
+	return unpack('<I', s)[0]
+
+def readlong(s):
+	return unpack('<Q', s)[0]
 
 childbufs = []
 for i in range(7):
@@ -25,13 +25,6 @@ def stepidx_(buf, idx, c):
 def checkidx_(buf, idx):
 	return (buf[idx + (idx << 8) + 1])
 
-def stepidx(buf, idx, c):
-	return BV2Int(buf[(idx * 2**8) + idx + BV2Int(c) + 2])
-
-def checkidx(buf, idx):
-	return BV2Int(buf[idx + (idx * 2**8) + 1])
-
-
 endstates = []
 for buf in childbufs:
 	try:
@@ -44,8 +37,6 @@ for buf in childbufs:
 
 endstates[1] = 33 #bug!
 assert len(endstates) == 7
-
-import graphAlgos
 
 
 def stepall(idxs, c):
@@ -62,10 +53,7 @@ def neighs(v):
 			ret.append(x)
 	return ret
 
-q = graphAlgos.dijkstra(neighs, (tuple([0]*7),), (tuple(endstates),), dist = lambda x,y: 1)
-# q = graphAlgos.bfs(neighs, (tuple([0]*7),), lambda path: path if path[0] == endstates else None)
-# q = graphAlgos.beamSearch(neighs, (tuple([0]*7),), (tuple(endstates),), 999, lambda x: 1)
-print q
+q = __import__('graphAlgos').dijkstra(neighs, (tuple([0]*7),), (tuple(endstates),), dist = lambda x,y: 1)
 
 s = ''
 for i in range(1,len(q)):
@@ -74,49 +62,3 @@ for i in range(1,len(q)):
 			s += c
 			break
 print s
-
-exit(0)
-
-from z3 import *
-from z3util import *
-
-solv = Solver()
-
-for i in range(7):
-	st = FakeString('child'+str(i), BitVecSort(32))
-	solv.add(len(st) == len(childbufs[i]))
-	for j in range(len(childbufs[i])):
-		solv.add(st[j] == childbufs[i][j])
-	childbufs[i] = st
-
-flag = FakeString('flag')
-
-solv.add(flag.isascii())
-
-# solv.add(len(flag) <= 32)
-# solv.add(0 < len(flag))
-# for buf in childbufs:
-# 	f = Function(buf.name+'steps', IntSort(), IntSort())
-# 	solv.add(f(-1) == 0)
-# 	x = Int('x')
-# 	solv.add(ForAllXInRange(0, len(flag), lambda x: f(x) == stepidx(buf, f(x-1), flag[x])))
-# 	solv.add(checkidx(buf, f(len(flag))) != 0)
-
-length = 1
-solv.add(len(flag) == length)
-for buf in childbufs:
-	prevstep = 0
-	for i in range(length):
-		prevstep = stepidx(buf, prevstep, flag[i])
-	thingy = checkidx(buf, prevstep)
-	solv.add(thingy != 0)
-
-
-for c in childbufs:
-	solv.add(c.constraint)
-solv.add(flag.constraint)
-
-print "Solving"
-print solv.check()
-print solv.model()
-print flag.stringInModel(solv.model())
